ff810f5c5a41bc012933bf22fc69874e5e124f7f
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-09-30 00:13:08 +0700

    fix: merge error

--- util.go
+++ util.go
@@ -15,19 +15,8 @@ func safeIntShift(data, shift, mod int) int {
 	return ((data % mod) + mod + (shift % mod) + mod) % mod
 }
 
-
-func isKoprima(key_a int, affineOption string) bool {
-	var m int
-	switch affineOption {
-	case "Alfabet (A-Z)":
-		m = 26
-	case "ASCII":
-		m = 255
-	}
-	return GCD(key_a, m) == 1
-
-func isAlphabet(char byte) bool  {
-	return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'B') 
+func isAlphabet(char byte) bool {
+	return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'B')
 }
 
 func isNumeric(char byte) bool {
